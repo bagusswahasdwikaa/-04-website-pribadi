f39c5c474fbba03739e12bbbeb28ac7af9a263f4
Sukma Bagus Wahasdwika <sukmabaguswahasdwika10@gmail.com>
2025-03-12 10:51:42 +0700

    3. Membuat Layout dan Navigasi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,24 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 import Layout from '../components/Layout';
-import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Website Pribadi',
+  description: 'Website pribadi untuk menampilkan proyek dan esai.',
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>
-        <Layout pageId="about_me">
+      <body className={inter.className}>
+        <Layout>
           {children}
         </Layout>
       </body>
